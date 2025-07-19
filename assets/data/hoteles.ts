c2883be05d7ce6a78ae0f929a7ce1bb821868e42
alex.saenz <alex.saenz@durango.gob.mx>
2025-07-18 19:41:00 -0600

    Auto-generated commit

---
+++ assets/data/hoteles.ts
@@ -0,0 +1,111 @@
+export const HOTELES_DATA = [
+  {
+    "ciudad": "Los cabos ",
+    "hoteles": []
+  },
+  {
+    "ciudad": "Rivera maya ",
+    "hoteles": []
+  },
+  {
+    "ciudad": "Puerto Vallarta ",
+    "hoteles": []
+  },
+  {
+    "ciudad": "Cancun",
+    "hoteles": [
+      {
+        "nombre": "Cid Cancun",
+        "descripcion": "",
+        "imagenes": [],
+        "id": "1eECQra1qodLngqsCvP1u7vGxwygFnUJl"
+      },
+      {
+        "nombre": "Hard rock",
+        "descripcion": "",
+        "imagenes": [],
+        "id": "1-GTj8udQ0h-ZZXBXlqkrjGzzM2sC7AED"
+      },
+      {
+        "nombre": "Moon palace",
+        "descripcion": "",
+        "imagenes": [],
+        "id": "1-EXHc2Fm098djDVTUtCzwmf023c7a-xu"
+      }
+    ]
+  },
+  {
+    "ciudad": "Mazatlan",
+    "hoteles": [
+      {
+        "nombre": "CID castilla",
+        "descripcion": "",
+        "imagenes": [],
+        "id": "1J9DXp400xThndxIDGfJkF5-ZOsbvjeDQ"
+      },
+      {
+        "nombre": "CID Moro",
+        "descripcion": "",
+        "imagenes": [],
+        "id": "1wmzMOSAj6f6YPVb8o4r2FGVmOfre0qay"
+      },
+      {
+        "nombre": "RIU",
+        "descripcion": "",
+        "imagenes": [],
+        "id": "16wGgKQgQUd37Za_rhgIil6fhWQo_g3uc"
+      },
+      {
+        "nombre": "Playa Mazatlán ",
+        "descripcion": "\nUbicado frente al mar, este resort todo incluido cuenta con albercas, acceso directo a la playa, restaurante buffet, bar y actividades para toda la familia.\n\n✓ Wi-Fi gratis\n✓ Habitaciones con vista al mar\n✓ Todo incluido\n✓ Ubicación: Zona Hotelera ",
+        "imagenes": [
+          "https://drive.google.com/thumbnail?id=1hkIhu23S2hQ1PVaXIP452QokcQ6rkCho&sz=w1000",
+          "https://drive.google.com/thumbnail?id=1phWmDVr46dcsSfS1uvuprILi-KhxenM3&sz=w1000",
+          "https://drive.google.com/thumbnail?id=1tpC-hVSBjTBaSt8mht3diVjeKbcHcsAw&sz=w1000",
+          "https://drive.google.com/thumbnail?id=13eW4Xs1Aw3X4fOMvxN9kiIhHIRVuZWhR&sz=w1000",
+          "https://drive.google.com/thumbnail?id=1HfmkOlGZ0NMeLYstI8p7s70fbePWabOJ&sz=w1000",
+          "https://drive.google.com/thumbnail?id=1AL8GPLR4AZCI4bpmrzW5Ueaxcf7IG8IH&sz=w1000",
+          "https://drive.google.com/thumbnail?id=1UgR7kaZG_i4CwzWnghEOIrhi6kOLNG66&sz=w1000"
+        ],
+        "id": "1-8iCk9sdogW3ck86KddbWIBsgH0FCQsX"
+      },
+      {
+        "nombre": "Costa de oro",
+        "descripcion": "\nUbicado frente al mar, este resort todo incluido cuenta con albercas, acceso directo a la playa, restaurante buffet, bar y actividades para toda la familia.\n\n✓ Wi-Fi gratis\n✓ Habitaciones con vista al mar\n✓ Todo incluido\n✓ Ubicación: Zona Hotelera ",
+        "imagenes": [
+          "https://drive.google.com/thumbnail?id=141HiO95w0mMRj_xbCJnZlENeUGBh8zDH&sz=w1000",
+          "https://drive.google.com/thumbnail?id=1U_WYfUyWF19oUE4XmZ9MkNWf4ss-hUOL&sz=w1000",
+          "https://drive.google.com/thumbnail?id=1ldMxyPf-fEu74wGcQoUeF2l33MJWHoIa&sz=w1000",
+          "https://drive.google.com/thumbnail?id=1FeuYPdpeGXI44ual_bH2GWB0ubtEZa0c&sz=w1000"
+        ],
+        "id": "1-3df4YhkRN-PQDojJaIzq76CFwPKOP5Q"
+      },
+      {
+        "nombre": "CID marina ",
+        "descripcion": "\nUbicado frente al mar, este resort todo incluido cuenta con albercas, acceso directo a la playa, restaurante buffet, bar y actividades para toda la familia.\n\n✓ Wi-Fi gratis\n✓ Habitaciones con vista al mar\n✓ Todo incluido\n✓ Ubicación: Zona Hotelera ",
+        "imagenes": [
+          "https://drive.google.com/thumbnail?id=1E2kjD1-H0n-qSgR_yKhxeP419dSJH1EM&sz=w1000",
+          "https://drive.google.com/thumbnail?id=1Z-ilS_3q7ykYrgUnCq09jZHkDCEdqPQO&sz=w1000",
+          "https://drive.google.com/thumbnail?id=19x6_Dp5m53heOusVATeAQ9zUGddKcHOh&sz=w1000",
+          "https://drive.google.com/thumbnail?id=1LDBcVbRCMiLnh05ynkmmPl_VAoqBcIR9&sz=w1000",
+          "https://drive.google.com/thumbnail?id=1IqFpTJ1KCaWCoHxeEvnKlw72evib1l5F&sz=w1000",
+          "https://drive.google.com/thumbnail?id=1oY_Ch6WeNqPjvRVyQ_gCfZtyZbUhUZGm&sz=w1000"
+        ],
+        "id": "1s9H87IHRSrdOuCDGMzuM_gkiV9p6MMLl"
+      },
+      {
+        "nombre": "Pueblo Bonito",
+        "descripcion": "\nUbicado frente al mar, este resort todo incluido cuenta con albercas, acceso directo a la playa, restaurante buffet, bar y actividades para toda la familia.\n\n✓ Wi-Fi gratis\n✓ Habitaciones con vista al mar\n✓ Todo incluido\n✓ Ubicación: Zona Hotelera ",
+        "imagenes": [
+          "https://drive.google.com/thumbnail?id=1pFyhjwp2RndevPdsosdQE4qmBYnfuID5&sz=w1000",
+          "https://drive.google.com/thumbnail?id=1Sf-mM7-BO35U8YKsd-iqqJ4UiPOFmOHj&sz=w1000",
+          "https://drive.google.com/thumbnail?id=1tVBocbkuBxNlrci1MXh7STwvvxD96HRo&sz=w1000",
+          "https://drive.google.com/thumbnail?id=1LwvybFSOeUSwi2jjkl7XuM7jLReoBv4c&sz=w1000",
+          "https://drive.google.com/thumbnail?id=1cXCuTHNtH8CIlIvQkgeO1MAHUyOyvNJa&sz=w1000",
+          "https://drive.google.com/thumbnail?id=1wk38rC3N_Qoyar_bZmLqW5k0exkJEgcC&sz=w1000"
+        ],
+        "id": "1RD9qc8c0OnuTGvU7CpCKO5-KKzlV39vv"
+      }
+    ]
+  }
+]
