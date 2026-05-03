21a721feda3a418a62ca2d8546ac9d8ed178026c
Alex Saenz <alexsaenz539@gmail.com>
2026-05-02 23:20:37 -0600

    Modulo de tipos de destinos

---
+++ src/app/components/admin/destinos/tipo-destinos/tipo-destinos.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tipo-destinos',
+  imports: [],
+  templateUrl: './tipo-destinos.component.html',
+  styleUrl: './tipo-destinos.component.scss'
+})
+export class TipoDestinosComponent {
+
+}
